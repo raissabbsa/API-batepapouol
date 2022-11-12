0db8c6e9bd0dde82a2c152b30950e89b3d1a8328
Raissa Barbosa <raissabbsa@gmail.com>
2022-11-12 11:08:07 -0300

    post messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {MongoClient} from "mongodb"
+import { MongoClient } from "mongodb"
 import joi from "joi"
 import dayjs from "dayjs";
 dotenv.config();
@@ -9,20 +9,30 @@ dotenv.config();
 const server = express();
 server.use(cors());
 server.use(json());
-const mongoClient = new MongoClient("mongodb://localhost:27017")
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 let db;
 
-mongoClient.connect().then(() => {
-  db= mongoClient.db("batePapoUol")
-})
+try{
+    await mongoClient.connect()
+    db=mongoClient.db("batePapoUol")
+} catch(err){
+    console.log(err)
+}
 
 const userSchema = joi.object({
     name: joi.string().required()
-  });
+});
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required()
+});
+const time = `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+
 
 server.post("/participants", async (req, res) => {
-    const {name} = req.body
-    const validation = userSchema.validate({name}, { abortEarly: true })
+    const { name } = req.body
+    const validation = userSchema.validate({ name }, { abortEarly: true })
 
     if (validation.error) {
         const err = validation.error.details.map(detail => detail.message)
@@ -31,54 +41,94 @@ server.post("/participants", async (req, res) => {
     }
 
     const listUsers = await db.collection("initParticipant").find().toArray()
-    const userFound = listUsers.find(user => user.name ===name)
-    if(userFound){
+    const userFound = listUsers.find(user => user.name === name)
+    if (userFound) {
         res.sendStatus(409)
         return
     }
-    try{
-        const names = await db.collection("initParticipant").insertOne({
+    try {
+        await db.collection("initParticipant").insertOne({
             name: name,
             lastStatus: Date.now()
         })
         await db.collection("message").insertOne({
-            name,
+            from: name,
             to: "todos",
             text: "entrando na sala...",
             type: "status",
-            time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+            time
         })
         res.sendStatus(201)
 
-    } catch(err) {
+    } catch (err) {
+        console.log(err)
+    }
+
+})
+
+server.get('/participants', async (req, res) => {
+
+    try {
+        const contacts = await db.collection("initParticipant").find().toArray()
+        res.send(contacts)
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+server.get("/messages", async (req, res) => {
+    const limit = parseInt(req.query.limit)
+    const user = req.headers.user
+    const messages = await db.collection("message").find({ to: "todos" }, { to: user }).toArray()
+
+    if (limit) {
+        const lastMessages = messages.slice(-limit)
+        res.send(lastMessages)
+    }
+    else {
+        res.send(messages)
+    }
+})
+
+server.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body
+    const from = req.headers.user;
+
+    const validation = messageSchema.validate({ to, text, type}, { abortEarly: false })
+    const isParticipant = await db.collection("initParticipant").findOne({ name: from })
+
+    if (validation.error || !isParticipant) {
+        res.sendStatus(422)
+        return
+    }
+
+    try {
+        await db.collection("message").insertOne({ from, to, text, type, time })
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err)
+    }
+
+})
+server.post("/status", async (req, res) => {
+    const user = req.headers.user
+    const isOnline = await db.collection("initParticipant").findOne({ name: user })
+    const lastStatus= Date.now()
+    console.log(isOnline)
+    try {
+        if (isOnline) {
+            //await db.collection("initParticipant").insertOne({ ...isOnline, lastStatus })
+            res.sendStatus(200)
+        }
+        else {
+            res.sendStatus(404)
+        }
+    }catch(err){
         console.log(err)
     }
     
 })
 
-server.get('/participants', async(req, res) => {
-
-    try{
-        const contacts = await db.collection("initParticipant").find().toArray()
-        res.send(contacts)
-    } catch(err) {
-    console.log(err)
-  }
-});
-
-server.get("/messages", async(req,res) => {
-    const limit = parseInt(req.query.limit)
-    const messages = await db.collection("message").find().toArray()
-    if(limit){
-        const lastMessages = messages.slice(-limit)
-        res.send(lastMessages)
-    }
-    else{
-        res.send(messages)
-    }
-})
-
-
 server.listen(5000, () => {
-  console.log("Rodando em http://localhost:5000");
+    console.log("Rodando em http://localhost:5000");
 });
